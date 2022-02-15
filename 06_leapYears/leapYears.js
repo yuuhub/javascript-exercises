fdc386d1c974ec5ac1d5ca027b72eb57e7d7ab1a
yuuhub <71030036+yuuhub@users.noreply.github.com>
2022-02-15 18:57:24 +0800

    Submit solutions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
 const leapYears = function(year) {
-    return ((year % 4 == 0) || (year % 400 == 0)) ? true : false;
+    if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        return true;
+    else
+        return false;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const c = ((temp - 32) * (5 / 9));
+  return Math.round(c * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const f = (temp * (9 / 5) + 32)
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
